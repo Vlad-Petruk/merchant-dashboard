3c88f4c51f267585d6300f3e5bbeae489270baed
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-26 22:18:57 +0300

    create app routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
+import { Greeting } from './components/greeting/Greeting.jsx';
+import { Registration } from './components/registration/Registration.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Greeting />
+      },
+      {
+        path: "/registration",
+        element: <Registration />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
