448d32223bad293cf7ca4b4da76acc0801bdc5ed
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-28 00:29:58 +0200

    add card for successfull Shopify connection

--- src/components/registration/registration_steps/ConnectShopify.jsx
+++ src/components/registration/registration_steps/ConnectShopify.jsx
@@ -17,6 +17,39 @@ function ListItem ({childHeader, childText}) {
     )
 }
 
+function ConnectShopifyCard() {
+    return(
+        <div className={cardStyles.card}>
+            <ChadIconContainer />
+            <h2>Connect your Shopify store</h2>
+            <p className={cardStyles.smallText}>Installs the Chad widget in your Shopify store and sets it up to display your customers’ order information and self-serve options.</p>
+
+            <div className={cardStyles.checklist}>
+                <ListItem childHeader={'Track orders and shipping'} childText={'Global coverage with 600+ couriers supported'}/>
+
+                <ListItem childHeader={'Manage orders'} childText={'Allow customers to track, return, exchange, or report problems with their orders'}/>
+
+                <ListItem childHeader={'Process returns and exchanges'} childText={'Automatically checks your store policy and existing inventory before resolving or escalating each request'}/> 
+            </div>
+            
+            <button type="button" className={cardStyles.create}>Connect Store</button>
+            <p className={cardStyles.login}>{"I don't use Shopify"}</p>
+        </div>
+    )
+}
+
+function SuccesfullShopifyConnection () {
+    return(
+        <div className={cardStyles.racoonCard}>
+            <img src="/racoon.png" alt="racoon" />
+            <h2>Store connected</h2>
+            <p className={cardStyles.smallText}>Chad is now able to manage customer support requests for [STORE-NAME].</p>
+            <button type="button" className={cardStyles.continueBtn}>Continue</button>
+            <p className={cardStyles.login}>Wrong store?<a href="">Connect another one</a></p>
+        </div>
+    )
+}
+
 function ConnectShopify() {
     const{ currentStep, handleBack, handleNext } = useRegistration()
 
@@ -27,24 +60,8 @@ function ConnectShopify() {
                 <button onClick={handleNext} disabled={currentStep === 4}>Next &gt;</button>
             </ProgressIndicator>
             <FormContainer>
-                <div className={cardStyles.card}>
-                    <ChadIconContainer />
-                    <h2>Connect your Shopify store</h2>
-                    <p className={cardStyles.smallText}>Installs the Chad widget in your Shopify store and sets it up to display your customers’ order information and self-serve options.</p>
-
-                    <div className={cardStyles.checklist}>
-                        <ListItem childHeader={'Track orders and shipping'} childText={'Global coverage with 600+ couriers supported'}/>
-
-                        <ListItem childHeader={'Manage orders'} childText={'Allow customers to track, return, exchange, or report problems with their orders'}/>
-
-                        <ListItem childHeader={'Process returns and exchanges'} childText={'Automatically checks your store policy and existing inventory before resolving or escalating each request'}/>
-                        
-                        
-                    </div>
-                    
-                    <button type="button" className={cardStyles.create}>Connect Store</button>
-                    <p className={cardStyles.login}>{"I don't use Shopify"}</p>
-                </div>
+                {/* <ConnectShopifyCard/> */}
+                <SuccesfullShopifyConnection />
             </FormContainer>
             
         </div>
