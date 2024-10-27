ef76e857fc547405b70e3b37d20a8b031bc4b3ee
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-27 11:07:41 +0200

    create reusable container with Chad

---
+++ src/components/registration/registration_steps/shared/ChadIconContainer.jsx
@@ -0,0 +1,10 @@
+import styles from './ChadIconContainer.module.css'
+
+export function ChadIconContainer() {
+    return(
+        <div className={styles.container}>
+            <img src="/octopus.png" alt="chad" />
+            <p>Chad</p>
+        </div>
+    )
+}
