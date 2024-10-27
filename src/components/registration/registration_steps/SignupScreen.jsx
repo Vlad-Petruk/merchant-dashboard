78fa369f2feac43a33662b50f416b37948674a9c
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-27 14:51:32 +0200

    add and style form and button on card

--- src/components/registration/registration_steps/SignupScreen.jsx
+++ src/components/registration/registration_steps/SignupScreen.jsx
@@ -13,7 +13,18 @@ function SignupScreen() {
                     <ChadIconContainer />
                     <h2>Welcome to Chad</h2>
                     <p className={cardStyles.smallText}>Go live in 10 minutes! Our self-service widget empowers your customers to manage orders and track shipments 24/7 without driving you crazy.</p>
+                    <form >
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" className={cardStyles.email} placeholder="megachad@trychad.com"/>
+                    <label htmlFor="name">Your name</label>
+                    <input type="text" name="name" className={cardStyles.name} placeholder="Mega Chad"/>
+                    <label htmlFor="password">Password</label>
+                    <input type="text" name="password" className={cardStyles.password} placeholder="Enter password"/>
+                    <button type="button">Create account</button>
+                    <p className={cardStyles.login}>Already have an account? <a href="">Login</a></p>
+                </form>
                 </div>
+                
             </FormContainer>
         </div>
     )
