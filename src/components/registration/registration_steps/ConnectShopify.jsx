ee9b81de4a037d640114f2a5775a282ee031b9ca
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-27 19:40:08 +0200

    add buttons on progressIndicator element

--- src/components/registration/registration_steps/ConnectShopify.jsx
+++ src/components/registration/registration_steps/ConnectShopify.jsx
@@ -1,7 +1,19 @@
+import { ProgressIndicator } from "./shared/ProgressIndicator";
+import { useRegistration } from "../../../hooks/RegistrationContext";
+import styles from '../Registration.module.css'
+
 function ConnectShopify() {
+    const{ currentStep, handleBack, handleNext } = useRegistration()
 
     return(
-        <></>
+        <div className={styles.stepScreen}>
+            <ProgressIndicator>
+                <button onClick={handleBack} disabled={currentStep === 1}>&lt; Back</button>
+                <button onClick={handleNext} disabled={currentStep === 4}>Next &gt;</button>
+            </ProgressIndicator>
+            <>Shopify</>
+        </div>
+        
     )
 }
 
