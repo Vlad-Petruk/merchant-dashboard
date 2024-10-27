f431fb76af83e4314da09517c5143d817a910685
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-27 11:08:32 +0200

    add Chad and text to card container

--- src/components/registration/registration_steps/SignupScreen.jsx
+++ src/components/registration/registration_steps/SignupScreen.jsx
@@ -1,5 +1,6 @@
 import { ProgressIndicator } from "./shared/ProgressIndicator"
 import { FormContainer } from "./shared/FormContainer"
+import { ChadIconContainer } from "./shared/ChadIconContainer"
 import styles from '../Registration.module.css'
 import cardStyles from './SignupScreen.module.css'
 function SignupScreen() {
@@ -9,9 +10,9 @@ function SignupScreen() {
             <ProgressIndicator/>
             <FormContainer>
                 <div className={cardStyles.card}>
-                    <div className="">
-
-                    </div>
+                    <ChadIconContainer />
+                    <h2>Welcome to Chad</h2>
+                    <p className={cardStyles.smallText}>Go live in 10 minutes! Our self-service widget empowers your customers to manage orders and track shipments 24/7 without driving you crazy.</p>
                 </div>
             </FormContainer>
         </div>
