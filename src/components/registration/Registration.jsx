ebbda98e151b766678eae15cb679fd81495e903d
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-27 19:40:54 +0200

    add conditional rendering of registration step

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -1,10 +1,33 @@
+import React from 'react';
+import { useRegistration } from '../../hooks/RegistrationContext';
+import { SignupScreen } from './registration_steps/SignupScreen';
+import { ConnectShopify } from './registration_steps/ConnectShopify';
+import { ConnectGmail } from './registration_steps/ConnectGmail';
+import { RegistrationComplete } from './registration_steps/RegistrationComplete';
 
-import { SignupScreen } from "./registration_steps/SignupScreen"
+const Registration = () => {
+  const { currentStep } = useRegistration();
 
-function Registration() {
-    return(
-        <SignupScreen />
-    )
-}
+  const renderStep = () => {
+    switch (currentStep) {
+      case 1:
+        return <SignupScreen />;
+      case 2:
+        return <ConnectShopify />;
+      case 3:
+        return <ConnectGmail />;
+      case 4:
+        return <RegistrationComplete/>;
+      default:
+        return <div>Error</div>;
+    }
+  };
 
-export { Registration }
+  return (
+    <div>
+      {renderStep()}
+    </div>
+  );
+};
+
+export { Registration };
