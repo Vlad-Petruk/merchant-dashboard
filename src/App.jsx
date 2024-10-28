6f2dfa5e5ca5843fad059e069382895e319e63cb
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-28 13:45:12 +0200

    create routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Registration } from './components/registration/Registration'
 import { RegistrationProvider } from './hooks/RegistrationContext';
-import './App.css'
+import {Greeting} from './components/greeting/Greeting'
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import './App.css';
 
 const App = () => {
-  return (
+  return ( 
     <RegistrationProvider>
-      <Registration />
+      <Router>
+            <Routes>
+                <Route path="/" element={<Registration />} />
+                <Route path="/greeting" element={<Greeting />} />
+            </Routes>
+        </Router>
     </RegistrationProvider>
   );
 };
