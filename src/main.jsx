d1c27cd20d6df6cdf903e85c6861b6b13420f304
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-28 12:24:07 +0200

    remove router

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx';
-import { Greeting } from './components/greeting/Greeting.jsx';
-import { Registration } from './components/registration/Registration.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Greeting />
-      },
-      {
-        path: "/registration",
-        element: <Registration />
-      }
-    ]
-  }
-])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </StrictMode>,
 )
