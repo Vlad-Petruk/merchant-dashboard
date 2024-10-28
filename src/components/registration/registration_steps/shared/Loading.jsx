0e04c00e89e091b6196fcc2f1df78601454200c5
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-28 16:22:08 +0200

    create loading components

---
+++ src/components/registration/registration_steps/shared/Loading.jsx
@@ -0,0 +1,12 @@
+import styles from "./Loading.module.css"
+
+function Loading () {
+
+    return (
+        <div className={styles.loading}>
+            <img src="/Loading.svg" alt="" />
+        </div>
+    )
+}
+
+export { Loading }
