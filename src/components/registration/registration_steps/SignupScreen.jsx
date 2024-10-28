e3fba5c443b1532685795f2f2e695221d27ae622
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-28 15:34:18 +0200

    add Login and Signup cards and manage active state

--- src/components/registration/registration_steps/SignupScreen.jsx
+++ src/components/registration/registration_steps/SignupScreen.jsx
@@ -5,64 +5,124 @@ import { useRegistration } from "../../../hooks/RegistrationContext"
 import { Loading } from "./shared/Loading"
 import { useState } from "react"
 import styles from '../Registration.module.css'
-import cardStyles from './SignupScreen.module.css'
+import cardStyles from './SignupScreen.module.css';
+
+function LoginCard ({email, setEmail, password, setPassword, handleSubmit, handleAction }) {
+    return(
+      <div className={cardStyles.card}>
+          <ChadIconContainer />
+          <h2>Welcome to Chad</h2>
+          <p className={cardStyles.smallText}>
+          Feeling ready to take on the day? Chad is too!
+          </p>
+          <form onSubmit={handleSubmit}>
+              <label htmlFor="email">Email</label>
+              <input
+                  type="email"
+                  name="email"
+                  className={cardStyles.email}
+                  placeholder="megachad@trychad.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                  type="password"
+                  name="password"
+                  className={cardStyles.password}
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+              />
+              <button type="submit" className={cardStyles.create} >Login</button>
+              <p className={cardStyles.login}>Do not have an account? <span onClick={()=>handleAction()} className={cardStyles.redirect}>Join the waitlist</span></p>
+          </form>
+      </div>
+    )
+  }
+
+function SignupCard ({email, setEmail, name, setName, password, setPassword, handleSubmit, handleAction }) {
+  return(
+    <div className={cardStyles.card}>
+        <ChadIconContainer />
+        <h2>Welcome to Chad</h2>
+        <p className={cardStyles.smallText}>
+            Go live in 10 minutes! Our self-service widget empowers your customers to manage orders and track shipments 24/7 without driving you crazy.
+        </p>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input
+                type="email"
+                name="email"
+                className={cardStyles.email}
+                placeholder="megachad@trychad.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+            />
+            <label htmlFor="name">Your name</label>
+            <input
+                type="text"
+                name="name"
+                className={cardStyles.name}
+                placeholder="Mega Chad"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+            />
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                name="password"
+                className={cardStyles.password}
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+            <button type="submit" className={cardStyles.create}>Create account</button>
+            <p className={cardStyles.login}>Already have an account? <span onClick={()=>handleAction()} className={cardStyles.redirect}>Login</span></p>
+        </form>
+    </div>
+  )
+}
 
 
 function SignupScreen() {
-    const{ currentStep, handleBack, handleNext,email, setEmail, name, setName, password, setPassword } = useRegistration()
+  const{ currentStep, handleBack, handleNext, signupConnectionState, setSignupConnectionState, email, setEmail, name, setName, password, setPassword } = useRegistration()
 
-    const handleSubmit = (e) => {
-        e.preventDefault(); 
+    const handleSubmit = () => { 
 
-        if (email && name && password) {
-            handleNext();
+        if (signupConnectionState === "initial") {
+            if (email && password) {
+                handleNext();
+                handleAction('connected')
+
+            }
+        } else if (signupConnectionState === "login") {
+            if (email && name && password) {
+                handleNext();
+                handleAction('connected')
+            }
         }
+    };
+
+    const handleAction = (nextState) => {
+        setSignupConnectionState(nextState);
     };
     return(
         <div className={styles.stepScreen}>
-            <ProgressIndicator/>
+            <ProgressIndicator>
+                <button onClick={handleBack} disabled={currentStep === 1}>&lt; Back</button>
+                <button onClick={handleNext} disabled={currentStep === 4||signupConnectionState === "initial"||signupConnectionState === "login"}>Next &gt;</button>
+            </ProgressIndicator>
             <FormContainer>
-                <div className={cardStyles.card}>
-                    <ChadIconContainer />
-                    <h2>Welcome to Chad</h2>
-                    <p className={cardStyles.smallText}>
-                        Go live in 10 minutes! Our self-service widget empowers your customers to manage orders and track shipments 24/7 without driving you crazy.
-                    </p>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            name="email"
-                            className={cardStyles.email}
-                            placeholder="megachad@trychad.com"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                        <label htmlFor="name">Your name</label>
-                        <input
-                            type="text"
-                            name="name"
-                            className={cardStyles.name}
-                            placeholder="Mega Chad"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            required
-                        />
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            name="password"
-                            className={cardStyles.password}
-                            placeholder="Enter password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                        <button type="submit" className={cardStyles.create}>Create account</button>
-                        <p className={cardStyles.login}>Already have an account? <span onClick={()=>handleBack()} className={cardStyles.redirect}>Login</span></p>
-                    </form>
-                </div>
+                {signupConnectionState === "initial"&& <LoginCard email={email} setEmail={setEmail} password={password} setPassword={setPassword} handleAction={()=>handleAction('login')} handleSubmit={()=>handleSubmit()}/>}
+                {signupConnectionState === "login"&& <SignupCard email={email} setEmail={setEmail} name={name} setName={setName} password={password} setPassword={setPassword} handleAction={()=>handleAction('initial')} handleSubmit={()=>handleSubmit()}/>}
+                {signupConnectionState === "connected"&& <LoginCard email={email} setEmail={setEmail} password={password} setPassword={setPassword} handleAction={()=>handleAction('login')} handleSubmit={()=>handleSubmit()}/>}
+                
             </FormContainer>
         </div>
     )
