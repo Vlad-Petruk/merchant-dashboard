83d6b5a85891f8188c628c68279a7a81fc1498a4
Vlad-Petruk <vladpetruk00@gmail.com>
2024-10-28 10:23:46 +0200

    move sate into context and prevent proceedng when empty fields

--- src/components/registration/registration_steps/SignupScreen.jsx
+++ src/components/registration/registration_steps/SignupScreen.jsx
@@ -1,10 +1,20 @@
 import { ProgressIndicator } from "./shared/ProgressIndicator"
 import { FormContainer } from "./shared/FormContainer"
 import { ChadIconContainer } from "./shared/ChadIconContainer"
+import { useRegistration } from "../../../hooks/RegistrationContext"
+import { useState } from "react"
 import styles from '../Registration.module.css'
 import cardStyles from './SignupScreen.module.css'
 function SignupScreen() {
+    const{ currentStep, handleBack, handleNext,email, setEmail, name, setName, password, setPassword } = useRegistration()
 
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+
+        if (email && name && password) {
+            handleNext();
+        }
+    };
     return(
         <div className={styles.stepScreen}>
             <ProgressIndicator/>
@@ -12,20 +22,44 @@ function SignupScreen() {
                 <div className={cardStyles.card}>
                     <ChadIconContainer />
                     <h2>Welcome to Chad</h2>
-                    <p className={cardStyles.smallText}>Go live in 10 minutes! Our self-service widget empowers your customers to manage orders and track shipments 24/7 without driving you crazy.</p>
-                    <form >
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" className={cardStyles.email} placeholder="megachad@trychad.com"/>
-                    <label htmlFor="name">Your name</label>
-                    <input type="text" name="name" className={cardStyles.name} placeholder="Mega Chad"/>
-                    <label htmlFor="password">Password</label>
-                    <input type="text" name="password" className={cardStyles.password} placeholder="Enter password"/>
-                    
-                    <button type="button" className={cardStyles.create}>Create account</button>
-                    <p className={cardStyles.login}>Already have an account? <a href="">Login</a></p>
-                </form>
+                    <p className={cardStyles.smallText}>
+                        Go live in 10 minutes! Our self-service widget empowers your customers to manage orders and track shipments 24/7 without driving you crazy.
+                    </p>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            className={cardStyles.email}
+                            placeholder="megachad@trychad.com"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                        <label htmlFor="name">Your name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            className={cardStyles.name}
+                            placeholder="Mega Chad"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                        />
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            className={cardStyles.password}
+                            placeholder="Enter password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <button type="submit" className={cardStyles.create}>Create account</button>
+                        <p className={cardStyles.login}>Already have an account? <a href="">Login</a></p>
+                    </form>
                 </div>
-                
             </FormContainer>
         </div>
     )
